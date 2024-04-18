5e1fc581d68591d801fbd19a5934c9633279ed85
17612937572 <2480243521@qq.com>
2024-04-18 18:21:35 +0800

    update: home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,32 @@
 <template>
-  <div>
-    <h2>Home</h2>
-  </div>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span class="card-title">Home</span>
+        <p class="card-desc">Create a variety of customized visualizations using d3.js and Ant V. </p>
+
+        <div class="mar-t-10">
+          <el-tag v-for="item in tags" :key="item" class="mar-r-10" type="info">{{ item }}</el-tag>
+
+        </div>
+      </div>
+    </template>
+  </el-card>
+
 </template>
 
 <script setup>
+import {reactive} from "vue";
 
+const tags = reactive(['Customization', 'Data visualization', 'd3.js', 'Ant V', 'Dynamic'])
 </script>
 
 <style scoped>
+.mar-r-10 {
+  margin-right: 10px;
+}
 
+.mar-r-10 {
+  margin-top: 10px;
+}
 </style>
